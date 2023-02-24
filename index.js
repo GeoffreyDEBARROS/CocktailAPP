c1c15e7e6c33af3963c5c96e32d1a99a5efcb7c7
DE BARROS Geoffrey <debarrosg01@gmail.com>
2023-02-24 11:56:31 +0100

    mobile first ok

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 let cocktailName = document.getElementById("cocktail-name");
 let cocktailIngredientsList = document.getElementById("ingredients-cocktail");
-const cocktailContainer = document.querySelector(".cocktail-container");
+let firstIngredient = document.getElementById("ingredient1");
+let secondIngredient = document.getElementById("ingredient2");
+let thirdIngredient = document.getElementById("ingredient3");
+let cocktailContainer = document.querySelector(".cocktail-container");
+let form = document.getElementById("form");
+let reset = document.getElementById("reset");
+
 let ingredient1 = "";
 let ingredient2 = "";
 let ingredient3 = "";
 
-const fetchCocktail = () => {
+const fetchCocktail = (ingredient1, ingredient2, ingredient3) => {
   let url = `https://api.api-ninjas.com/v1/cocktail?ingredients=${ingredient1}`;
   if (ingredient2) {
     url += `,${ingredient2}`;
@@ -13,7 +19,6 @@ const fetchCocktail = () => {
   if (ingredient3) {
     url += `,${ingredient3}`;
   }
-
   fetch(url, {
     headers: { "X-Api-Key": "aXJ9lIAmt0zbmZLVIHPi5w==8w4ZS1jO9BVg9tYq" },
   })
@@ -33,16 +38,39 @@ const fetchCocktail = () => {
                 .map((ingredient) => `<li>${ingredient}</li>`)
                 .join("")}
             </ul>
-            <p class="card_instructions">
-                ${instructions}
-            </p>
           </div>
+          <p class="card-instructions">
+                ${instructions}
+          </p>
         </div>
         `;
         cocktailCards += card;
       });
-      cocktailContainer.innerHTML = cocktailCards;
+      if (cocktailCards === "") {
+        cocktailContainer.innerHTML = "<h3>No cocktails found</h3>";
+      } else {
+        cocktailContainer.innerHTML = cocktailCards;
+      }
     })
     .catch((error) => console.error(error));
 };
-fetchCocktail();
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  ingredient1 = firstIngredient.value;
+  ingredient2 = secondIngredient.value;
+  ingredient3 = thirdIngredient.value;
+  fetchCocktail(ingredient1, ingredient2, ingredient3);
+});
+
+const clearCocktailContainer = () => {
+  const cocktailContainer = document.querySelector(".cocktail-container");
+  cocktailContainer.innerHTML = "";
+  firstIngredient.value = "";
+  secondIngredient.value = "";
+  thirdIngredient.value = "";
+};
+
+reset.addEventListener("click", () => {
+  clearCocktailContainer();
+});
